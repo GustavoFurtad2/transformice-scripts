599fc0766c3f2323a8e873f3d77aaec91ab0b03e
Gustavo Furtado <146868632+GustavoFurtad2@users.noreply.github.com>
2024-01-12 18:25:21 -0300

    Create res.lua

---
+++ #Micequest/src/res.lua
@@ -0,0 +1,21 @@
+local RES = {
+    Icons = {
+      Health = {
+        ["0"   ] = "18cbc58c299.png",
+        ["0.25"] = "18cbc5910bb.png",
+        ["0.50"] = "18cbc595ed5.png",
+        ["0.75"] = "18cbc59acff.png",
+        ["1"   ] = "18cbc59fb0d.png",
+      },
+    },
+    SPRITES = {
+      MICE = {
+        ["DOWN" ] = "17eed490d4e.png",
+        ["RIGHT"] = "17eed4e062c.png",
+        ["UP"   ] = "17eed52eb45.png",
+      },
+      MAP = {
+        ["1"] = "17b9e02a563.png",
+      }
+    },
+}
